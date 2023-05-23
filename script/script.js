749955d76c7b5383204da116cb1f3e80f5013be7
0VitorGabriel <victoropo64@outlook.com>
2023-05-22 20:20:20 -0400

    fiz duas novas funcões para ficar mais legivel

--- script/script.js
+++ script/script.js
@@ -30,32 +30,35 @@ function show_user_data(user) {
         if (user.login === undefined) {
             throw Error('usuario não encontrado')
         } else {
-            const h3 = document.createElement('h3')
-            h3.setAttribute('class', 'text-center')
-            h3.innerHTML = `login: ${user.login}`
-            console.log(user)
-
-            const img = document.createElement('img')
-            img.setAttribute('src', `${user.avatar_url}`)
-            img.setAttribute('alt', 'foto do usuario')
-            img.setAttribute('class', 'rounded mx-auto d-block w-25 p-3')
-
-            const divFollowing = document.createElement('p')
-            divFollowing.innerHTML = `seguindo: ${user.following}`
-            divFollowing.setAttribute('style', 'float: left;')
-
-            const divFollowers = document.createElement('p')
-            divFollowers.innerHTML = `seguidores: ${user.followers}`
-            divFollowers.setAttribute('style', 'float: right;')
-
-            div.appendChild(divFollowing)
-            div.appendChild(divFollowers)
-            div.appendChild(h3)
-            div.appendChild(img)
+            createElementsUser(user)
         }
     } catch (error) {
         div.innerHTML = `<p style="text-align: center;">${error.message}</p>`
     }
+}
+
+function createElementsUser(user) {
+    const h3 = document.createElement('h3')
+    h3.setAttribute('class', 'text-center')
+    h3.innerHTML = `login: ${user.login}`
+
+    const img = document.createElement('img')
+    img.setAttribute('src', `${user.avatar_url}`)
+    img.setAttribute('alt', 'foto do usuario')
+    img.setAttribute('class', 'rounded mx-auto d-block w-25 p-3')
+
+    const divFollowing = document.createElement('p')
+    divFollowing.innerHTML = `seguindo: ${user.following}`
+    divFollowing.setAttribute('style', 'float: left;')
+
+    const divFollowers = document.createElement('p')
+    divFollowers.innerHTML = `seguidores: ${user.followers}`
+    divFollowers.setAttribute('style', 'float: right;')
+
+    div.appendChild(divFollowing)
+    div.appendChild(divFollowers)
+    div.appendChild(h3)
+    div.appendChild(img)
 }
 
 async function findRepositories(user) {
@@ -71,22 +74,27 @@ async function findRepositories(user) {
 }
 
 function show_data_repositories(repos) {
+    const ul = document.querySelector('ul')
+    ul.innerHTML = ''
+    const h4 = document.querySelector('h4').style.display = 'inline'
+
     try {
-        const ul = document.querySelector('ul')
-        ul.innerHTML = ''
-        const h4 = document.querySelector('h4').style.display = 'inline'
 
-        repos.map((repos) => {
-            const li = document.createElement('li')
-
-            li.innerHTML = `${repos.name}`
-
-            ul.appendChild(li)
-        })
+        createElementsRepositories(repos, ul)
         
     } catch (error) {
         const divRepositories = document.querySelector('div#repositories')
 
         divRepositories.innerHTML = 'nenhum repositório encontrado'
     }
+}
+
+function createElementsRepositories(repos, ul) {
+    repos.map((repos) => {
+        const li = document.createElement('li')
+    
+        li.innerHTML = `${repos.name}`
+    
+        ul.appendChild(li)
+    })
 }
